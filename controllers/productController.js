423ce00bf4f6288b27cd34e253da0e791ce705ea
Tharidu Sathsara <tharidusathsara200@gmail.com>
2025-01-19 22:50:47 +0530

    fix error

--- controllers/productController.js
+++ controllers/productController.js
@@ -20,7 +20,7 @@ export function addProduct(req,res){
     const newProduct = new Product(data);
     newProduct.save()
     .then(()=>{
-        res.json({message:"Prduct added successfully"});
+        res.json({message:"Product added successfully"});
     })
     .catch((error)=>{
         res.status(500).json({error:"Product addition failed"});
