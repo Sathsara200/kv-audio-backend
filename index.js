b1d2222816f08e5a63473804ad8132ed0b86fab9
Tharidu Sathsara <tharidusathsara200@gmail.com>
2025-03-06 01:14:07 +0530

    order creation api🤞

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import dotenv from "dotenv";
 import reviewRouter from "./routes/reviewRouter.js";
 import inquiryRouter from "./routes/InquiryRouter.js";
 import cors from "cors";
+import orderRouter from "./routes/orderRouter.js";
 
 dotenv.config();
 
@@ -50,6 +51,7 @@ app.use("/api/users",userRouter);
 app.use("/api/products",productRouter);
 app.use("/api/reviews",reviewRouter);
 app.use("/api/inquiries",inquiryRouter);
+app.use("/api/orders",orderRouter);
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
